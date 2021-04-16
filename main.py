5be58d701a30b674a59a613a946397a317baa7af
Emmett Ling <emmettl@berkeley.edu>
2021-04-16 09:01:05 -0700

    Added Point object and refactored

--- main.py
+++ main.py
@@ -8,76 +8,82 @@ class Headquarters:
     def get_station(self, station_id):
         return self.stations[station_id]
 
-    def already_has(self, station_id):
-        return station_id in self.stations
-
     ############### SCREENING QUESTION METHODS #################################
 
-    def lowest_temperature(self):
-        lowest_data_point = [-1, -1, float('inf')]
+    def fetch_lowest_temperature_station_date(self):
+        lowest_recorded = Point([-1, -1, float('inf')])
 
         for station_id in self.stations:
             station = self.get_station(station_id)
-            local_lowest, local_date = station.find_lowest_temperature_and_date()
-            if local_lowest < DataHelper.temperature(lowest_data_point):
-                lowest_data_point = [station_id, local_date, local_lowest]
 
-        return {"Station ID:": DataHelper.station_id(lowest_data_point), "Date:": DataHelper.date(lowest_data_point)}
+            station_lowest = station.find_lowest_temperature_and_date()
 
-    # OOP solutions just for fun
+            if station_lowest.temperature < lowest_recorded.temperature:
+                lowest_recorded = station_lowest
+
+        return {"Station ID:": lowest_recorded.station_id, "Date:":lowest_recorded.date}
+
+    # OOP solutions
 
     # A simple solution which measures the lowest temperature as data is imported
     def lowest_temperature_oop_solution(self):
-        return DataHelper.lowest_temperature()
+        return DataHelper.lowest_temperature_station_date()
 
-    # Another solution which I re-implemented later as this one was trivially easy
+    # Another OOP solution
     def fluctuation_station_oop_solution(self):
-        highest_fluctuating_station_id = 68
-        highest_fluctation = 0
+        highest_station = Station(-1)
 
-        for station_id in self.stations:
-            station_fluctuation = self.get_station(station_id).get_fluctuation()
-            if station_fluctuation > highest_fluctation:
-                highest_fluctuating_station_id = station_id
-                highest_fluctation = station_fluctuation
-        print("Station " + str(highest_fluctuating_station_id) + " recorded the highest fluctuation of " + str(highest_fluctation))
-        return highest_fluctuating_station_id
-    ############### SETUP METHODS ##############################################
+        for station in self.stations.values():
+            if station.fluctuation > highest_station.fluctuation:
+                highest_station = station
 
-    def add_station(self, station_id, data_point):
+        print("Station " + str(highest_station.station_id) + " recorded the highest fluctuation of " + str(highest_station.fluctuation))
+        return highest_station.station_id
+
+    ############### IMPORT METHODS ##############################################
+
+    def add_point(self, point):
+        station_id = point.station_id
+        if self.already_has(station_id):
+            self.add_data(station_id, point)
+        else:
+            new_station = self.add_station(station_id, point)
+            new_station.add_data(point)
+
+    def already_has(self, station_id):
+        return station_id in self.stations
+
+    def add_station(self, station_id, point):
         new_station = Station(station_id)
         self.stations[station_id] = new_station
+        new_station.add_data(point)
+
         return new_station
 
-    def add_data(self, station_id, data_point):
-        self.get_station(station_id).add_data(data_point)
+    def add_data(self, station_id, point):
+        self.get_station(station_id).add_data(point)
 
 class Station:
     def __init__(self, station_id):
-        self.station_id = int(station_id)
-        self.station_data = []
-        self.fluctuation = 0
+        self.station_id     = int(station_id)
+        self.station_data   = []
+        self.fluctuation    = 0
 
-    def add_data(self, data_point):
-        self.fluctuation += abs(DataHelper.temperature(data_point))
-        self.station_data.append(data_point)
+    def add_data(self, point):
+        self.fluctuation += abs(point.temperature)
+        self.station_data.append(point)
 
     def get_station_data(self):
         return self.station_data
 
-    def get_fluctuation(self):
-        return self.fluctuation
-
     def find_lowest_temperature_and_date(self):
-        lowest_temperature = float('inf')
-        lowest_temperature_date = 0
+        lowest_recorded_point = Point([-1, -1, float('inf')])
 
-        for data_point in self.station_data:
-            data_temperature = DataHelper.temperature(data_point)
-            if data_temperature < lowest_temperature:
-                lowest_temperature = data_temperature
-                lowest_temperature_date = DataHelper.date(data_point)
-        return lowest_temperature, lowest_temperature_date
+        for point in self.station_data:
+            if point.temperature < lowest_recorded_point.temperature:
+                lowest_recorded_point = point
+
+        return lowest_recorded_point
 
     def __repr__(self):
         return "Station " + str(int(self.station_id))
@@ -85,36 +91,32 @@ class Station:
     def __str__(self):
         return "Station " + str(int(self.station_id))
 
+class Point:
+    def __init__(self, data_point):
+        self.station_id  =   data_point[0]
+        self.date        =   data_point[1]
+        self.temperature =   data_point[2]
+
+
 class DataHelper:
-    lowest_recorded_temperature = [-1, -1, float('inf')]
+    lowest_recorded_point = Point([-1, -1, float('inf')])
 
-    def station_id(data_point):
-        return int(data_point[0])
-
-    def date(data_point):
-        return data_point[1]
-
-    def temperature(data_point):
-        return data_point[2]
-
-    def parse(row):
-        # >>> row = ['2, 2020.123, 4']
-        # >>> parse(row)
-        # [2, 2020.123, 4]
-
+    def pointify(row):
         row = row[0].split(',')
         row = list(map(lambda item: float(item), row))
 
-        return row
+        return Point(row)
 
-    def consider_lowest(data_point):
-        if DataHelper.temperature(data_point) < DataHelper.temperature(DataHelper.lowest_recorded_temperature):
-            DataHelper.lowest_recorded_temperature = data_point
+    def consider_lowest(point):
+        if point.temperature < DataHelper.lowest_recorded_point.temperature:
+            DataHelper.lowest_recorded_point = point
 
-    def lowest_temperature():
-        station_id = DataHelper.station_id(DataHelper.lowest_recorded_temperature)
-        date = DataHelper.date(DataHelper.lowest_recorded_temperature)
-        temperature_c = DataHelper.temperature(DataHelper.lowest_recorded_temperature)
+    def lowest_temperature_station_date():
+        lowest_point = DataHelper.lowest_recorded_point
+
+        station_id =    lowest_point.station_id
+        date =          lowest_point.date
+        temperature_c = lowest_point.temperature
 
         print("The lowest temperature recorded is " + str(temperature_c) + " Celsius, recorded at Station " + str(station_id) + " on " + str(date))
         return {"Station ID:": station_id, "Date:": date}
@@ -127,19 +129,15 @@ with open('data.csv', newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     next(csvreader)
     for row in csvreader:
-        data_point = DataHelper.parse(row)
-        station_id = DataHelper.station_id(data_point)
+        point = DataHelper.pointify(row)
 
-        DataHelper.consider_lowest(data_point)
+        hq.add_point(point)
 
-        if hq.already_has(station_id):
-            hq.add_data(station_id, data_point)
-        else:
-            new_station = hq.add_station(station_id, data_point)
-            new_station.add_data(data_point)
+        # 'sidecar-consumer' for Part 1's OOP solution
+        DataHelper.consider_lowest(point)
 
 
-print(hq.lowest_temperature())
+print(hq.fetch_lowest_temperature_station_date())
 print("OOP Solutions")
 print(hq.lowest_temperature_oop_solution())
 print(hq.fluctuation_station_oop_solution())
